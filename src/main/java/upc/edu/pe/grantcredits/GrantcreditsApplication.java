9c8ba7dacb05582efd4dc940ca5f42e79ea624a2
Bengalaz <ffreddy8900@gmail.com>
2020-10-19 19:44:18 -0500

    primera version

---
+++ src/main/java/upc/edu/pe/grantcredits/GrantcreditsApplication.java
@@ -0,0 +1,13 @@
+package upc.edu.pe.grantcredits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrantcreditsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrantcreditsApplication.class, args);
+    }
+
+}
